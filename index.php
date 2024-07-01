a5ed6d660015a5f83cbaf8ea5749dee5b16c6b22
anferalta <150276760+anferalta@users.noreply.github.com>
2024-07-01 19:04:16 +0100

    criei função CPF

--- index.php
+++ index.php
@@ -8,15 +8,25 @@
     <body>
         <?php
         //Arquivo index responsável pela inicialização do sistema
-
         require_once 'sistema/configuracao.php';
         include_once 'helpers.php';
-           
-        $numero = 5;
-        while ($numero <= 10){
-            echo $numero++;
-        }
-            
+
+        $cpf = '004.348.491-38';
+        
+        var_dump(validarCPF($cpf));
+        //echo $limparNumero = preg_replace('/[^0-9]/', ' ', $cpf);
+
+        //for ($t = 9; $t < 11; $t++) {
+        //    for ($d = 0, $c = 0; $c < $t; $c++) {
+        //        $d += $cpf[$c] * (($t + 1) - $c);
+        //    }
+        //    $d = ((10 * $d) % 11) % 10;
+        //    if ($cpf[$c] != $d) {
+        //        echo 'CPF INVÁLIDO';
+        //    } else {
+        //        echo 'CPF VÁLIDO';
+        //    }
+        //}
         
         ?>	
     </body>
