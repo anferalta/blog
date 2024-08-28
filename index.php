760ce55698428cfeb7c32b471b6b7d493fee2ba8
anferalta <150276760+anferalta@users.noreply.github.com>
2024-08-28 10:07:32 +0100

    criei a função  session

--- index.php
+++ index.php
@@ -11,8 +11,20 @@
         //Arquivo index responsavel pela inicialização do sistema
 
         require 'vendor/autoload.php';
-        require 'rotas.php';     
-                
+        //require 'rotas.php';     
+        
+        $session = new sistema\Nucleo\Sessao();
+        
+        //$session->criar('nome', 'Antonio Almeida');
+        var_dump($session->carregar());
+        echo '<hr>';
+        var_dump($session->checar('nome'));
+        echo '<hr>';
+        //$session->limpar('usuario');
+        //var_dump($session->checar('usuario'));
+        echo '<hr>';
+        $session->deletar();
+        
         ?>	
 
 </html>
