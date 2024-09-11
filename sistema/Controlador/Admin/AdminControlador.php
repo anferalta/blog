19711ba4ff152b28b70ca99ae4f1b0192427211d
anferalta <150276760+anferalta@users.noreply.github.com>
2024-09-11 11:54:30 +0100

    update login

--- sistema/Controlador/Admin/AdminControlador.php
+++ sistema/Controlador/Admin/AdminControlador.php
@@ -4,6 +4,8 @@ namespace sistema\Controlador\Admin;
 
 use sistema\Nucleo\Controlador;
 use sistema\Nucleo\Helpers;
+use sistema\Controlador\UsuarioControlador;
+use sistema\Nucleo\Sessao;
 
 /**
  * Description of AdminControlador
@@ -12,14 +14,20 @@ use sistema\Nucleo\Helpers;
  */
 class AdminControlador extends Controlador
 {
+    protected $usuario;
+
+
     public function __construct()
     {
         parent::__construct('templates/admin/views');
         
-        $usuario = false;
+        $this->usuario = UsuarioControlador::usuario();
         
-        if(!$usuario){
+        if($this->usuario OR $this->usuario->level != 3){
             $this->mensagem->erro('Faça login para acessar o painel de controle!')->flash();
+            
+            $sessao = new Sessao();
+            $sessao->limpar('usuarioId');
             
             Helpers::redirecionar('admin/login');
         }
--- sistema/Controlador/Admin/AdminLogin.php
+++ sistema/Controlador/Admin/AdminLogin.php
@@ -4,6 +4,8 @@ namespace sistema\Controlador\Admin;
 
 use sistema\Nucleo\Controlador;
 use sistema\Nucleo\Helpers;
+use sistema\Modelo\UsuarioModelo;
+
 
 /**
  * Description of AdminControlador
@@ -20,7 +22,20 @@ class AdminLogin extends Controlador
     
     public function login(): void
     {
+        $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
         
+        if (isset($dados)){
+            if (in_array('', $dados)){
+                $this->mensagem->alerta('Todos os campos são obrigatórios!')->flash();
+           }else {
+                $usuario = (new UsuarioModelo())->login($dados, 3);
+                
+                if($usuario){
+                    Helpers::redirecionar('admin/login');
+                }
+            }
+        }
+    
         echo $this->template->renderizar('login.html', []);
     }
 }
