7fbfa4b5534bd79ee35cd37054704aab5e52e8ca
anferalta <150276760+anferalta@users.noreply.github.com>
2024-10-04 22:33:59 +0100

    criei função upload

---
+++ sistema/biblioteca/Upload.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace sistema\biblioteca;
+
+/**
+ * Description of upload
+ *
+ * @author Administrador
+ */
+class Upload
+{
+    private $arquivo;
+    private $nome;
+    
+    public function __construct()
+    {
+        
+    }
+    
+    public function arquivo()
+    {
+        
+    }
+}
