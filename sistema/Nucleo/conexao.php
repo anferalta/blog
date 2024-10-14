8b4e86ec671ec4877472fc5aadc7b6c8070003d2
anferalta <150276760+anferalta@users.noreply.github.com>
2024-10-14 22:40:40 +0100

    upload com a class verot

--- sistema/Nucleo/conexao.php
+++ sistema/Nucleo/conexao.php
@@ -22,7 +22,7 @@ class Conexao
 
             try {
                 self::$instancia = new PDO('mysql:host=' . DB_HOST . ';port=' . DB_PORTA . ' ;dbname=' . DB_NOME, DB_USUARIO, DB_SENHA, [
-                    // garante que o charset do PDO seja o mesmp do banco de dados
+                    // garante que o charset do PDO seja o mesmo do banco de dados
                     PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
                     // todo erro através da PDO será uma execução
                     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
--- sistema/biblioteca/Upload.php
+++ sistema/biblioteca/Upload.php
@@ -3,18 +3,19 @@
 namespace sistema\biblioteca;
 
 /**
- * Description of upload
- *
+ * Retorna o resultado
+ *@return string|null = Nome do arquivo
  * @author Administrador
  */
 class Upload
 {   
-    private $directorio;
-    private $arquivo;
-    private $nome;
-    private $subDirectorio;
-    public $resultado;
-    public $erro;
+    private ?string $directorio;
+    private ?array $arquivo;
+    private ?string $nome;
+    private ?string $subDirectorio;
+    private ?int $tamanho;
+    private ?string $resultado;
+    private ?string $erro;
     
     public function getResultado(): ?string
     {
@@ -35,7 +36,7 @@ class Upload
         }
     }
     
-    public function arquivo(array $arquivo, string $nome = null, string $subDirectorio = null)
+    public function arquivo(array $arquivo, string $nome = null, string $subDirectorio = null, int $tamanho = null)
     {
         $this->arquivo = $arquivo;
         
@@ -43,19 +44,43 @@ class Upload
         
         $this->subDirectorio = $subDirectorio ?? 'arquivos';
         
-        $this->criarSubDirectorio();
-        $this->renomearArquivo();
-        $this->moverArquivo();
-    }
+        $extensao = pathinfo($this->arquivo['name'], PATHINFO_EXTENSION);
+        
+        $this->tamanho = $tamanho ?? 1;
+        
+        $extensoesValidas = [
+            'pdf',
+            'png',
+            'docx'
+        ];
+        
+        $tiposValidos = [
+            'application/pdf',
+            'text/plain',
+            'image/png'
+        ];
+        
+        if (!in_array($extensao, $extensoesValidas)){
+            $this->erro = 'extensão não permitida. voçê só pode enviar .'.implode(' .', $extensoesValidas);
+        }elseif (!in_array($this->arquivo['type'], $tiposValidos)) {
+            $this->erro = 'Tipo não permitido';
+        }elseif ($this->arquivo['size'] > $this->tamanho * (1024 * 1024)) {
+            $this->erro = "arquivo muito grande, permitido {$this->tamanho}MB seu arquivo tem {$this->arquivo['size']}";
+        }else {
+            $this->criarSubDirectorio();
+            $this->renomearArquivo();
+            $this->moverArquivo();
+        }
+     }
     
-    public function criarSubDirectorio(): void
+     private function criarSubDirectorio(): void
     {
         if (!file_exists($this->directorio.DIRECTORY_SEPARATOR.$this->subDirectorio)  && !is_dir($this->directorio. DIRECTORY_SEPARATOR.$this->subDirectorio)){
             mkdir($this->directorio.DIRECTORY_SEPARATOR.$this->subDirectorio, 0755);
         }
     }
     
-    public function renomearArquivo(): void
+    private function renomearArquivo(): void
     {
         echo $arquivo = $this->nome.strrchr($this->arquivo['name'], '.');
         if (file_exists($this->directorio.DIRECTORY_SEPARATOR.$this->subDirectorio. DIRECTORY_SEPARATOR.$arquivo)){
@@ -64,7 +89,7 @@ class Upload
         $this->nome = $arquivo;
     }
     
-    public function moverArquivo(): void
+    private function moverArquivo(): void
     {
         if (move_uploaded_file($this->arquivo['tmp_name'], $this->directorio. DIRECTORY_SEPARATOR.$this->subDirectorio.DIRECTORY_SEPARATOR.$this->nome)){
             $this->resultado = $this->nome;
